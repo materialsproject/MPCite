85a26d8f8dedab67445ee4ed47546210a43dbc0e
Patrick Huck <phuck@lbl.gov>
2016-03-31 22:43:08 -0700

    adjust dir structure to new MPCite repo

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 source $HOME/.credentials
 source $HOME/.virtualenvs/env_mp_osti_doi/bin/activate
-cd $HOME/MPWorks
+cd $HOME/MPCite
 export PYTHONPATH=`pwd`:$PYTHONPATH
-mgbuild run -v mpworks.osti_doi.builders.DoiBuilder nmats=25 dois=dois.json materials=materials.json
-git add mpworks/osti_doi/dois.json
+mgbuild run -v mpcite.builders.DoiBuilder nmats=25 dois=dois.json materials=materials.json
+git add dois.json
 git commit -m "osti_doi: new dois backup"
-git push origin osti_doi
+#git push origin osti_doi
