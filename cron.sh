87078a3a99a528e3bb7647dea100ad158b64593f
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-08-11 16:52:43 -0700

    cron script added

---
+++ cron.sh
@@ -0,0 +1,4 @@
+source /Users/michaelwu/Desktop/projects/MPCite/activate_conda
+conda activate MPCite;
+mpcite --config_file_path /Users/michaelwu/Desktop/projects/MPCite/files/config_prod.json;
+conda deactivate
