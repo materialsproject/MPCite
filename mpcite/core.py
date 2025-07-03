a7473264a37d8e0c8623bbd0804498b365618990
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2025-07-03 14:05:10 -0700

    find_out_of_date func

--- mpcite/core.py
+++ mpcite/core.py
@@ -7,21 +7,44 @@ from pymongo import MongoClient
 OstiID: TypeAlias = int
 
 
-def find_out_of_date_records(
+def find_out_of_date_doi_entries(
     client: MongoClient,
     robocrys_db: str,
     robocrys_collection: str,
     doi_db: str,
-    doi_collection,
+    doi_collection: str,
 ) -> list[OstiID]:
-    robocrys = client.robocrys_db.robocrys_collection
-    doi = client.doi_db.doi_collection
+    robocrys = client[robocrys_db][robocrys_collection]
+    dois = client[doi_db][doi_collection]
 
-    out_of_data_osti_ids = []
+    latest_doi = next(
+        dois.aggregate(
+            [
+                {"$project": {"_id": 0, "date_record_last_updated_on_Elink": 1}},
+                {"$sort": {"date_record_last_updated_on_Elink": -1}},
+                {"$limit": 1},
+            ]
+        )
+    )["date_record_last_updated_on_Elink"]
 
-    # robocrys docs newer than in doi
+    material_ids_to_update = list(
+        map(
+            lambda x: x["material_id"],
+            robocrys.find(
+                {"last_updated": {"$gt": latest_doi}}, {"_id": 0, "material_id": 1}
+            ),
+        )
+    )
 
-    return out_of_data_osti_ids
+    return list(
+        map(
+            lambda x: x["osti_id"],
+            dois.find(
+                {"material_id": {"$in": material_ids_to_update}},
+                {"_id": 0, "osti_id": 1},
+            ),
+        ),
+    )
 
 
 def update_existing_osti_record(*args, **kwargs) -> RecordResponse: ...
