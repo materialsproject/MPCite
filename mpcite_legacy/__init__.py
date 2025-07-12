52382ff4345f4cb8506af79e8c0db6e63b24a99e
HugoOnghai <99376417+HugoOnghai@users.noreply.github.com>
2025-07-11 17:01:29 -0700

    Merged upstream (#1)
    * move old code to 'legacy'
    
    * setup project using uv
    
    * add license
    
    * testing skeleton
    
    * gh actions skeleton
    
    * remove old reqs file to prevent dependabot alerts
    
    ---------
    
    Co-authored-by: Tyler Mathis <35553152+tsmathis@users.noreply.github.com>

--- mpcite_legacy/__init__.py
+++
@@ -1,8 +0,0 @@
-"""The Materials Project's Citation Framework"""
-
-__version__ = "1.0.0"
-__url__ = "https://github.com/materialsproject/MPCite"
-__author__ = "Patrick Huck & Michael Wu"
-__email__ = "phuck@lbl.gov"
-__copyright__ = "Copyright 2020, The Materials Project"
-__maintainer__ = "Patrick Huck"
