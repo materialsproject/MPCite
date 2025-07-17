d7a7e393655f82ab492346f31881c4be220d7c28
HugoOnghai <onghaih22@gmail.com>
2025-07-17 16:25:37 -0700

    Testing pre-commit and updated lint.yml to disregard legacy files

--- src/mp_cite/core.py
+++ src/mp_cite/core.py
@@ -16,7 +16,6 @@ def find_out_of_date_doi_entries(
     doi_db: str,
     doi_collection: str,
 ) -> list[OstiID]:
-<<<<<<< HEAD
     """
     find_out_of_date_doi_entries queries MP's mongo collections to find all robocrys documents that were updated less recently than the latest doi document
 
@@ -29,8 +28,6 @@ def find_out_of_date_doi_entries(
 
     returns a list containing all OSTI IDs associated with out-of-date doi entries.
     """
-=======
->>>>>>> 5fa46e4 (Merged upstream (#1))
     robocrys = rc_client[robocrys_db][robocrys_collection]
     dois = doi_client[doi_db][doi_collection]
 
@@ -65,7 +62,6 @@ def find_out_of_date_doi_entries(
 
 
 def update_existing_osti_record(
-<<<<<<< HEAD
     elinkapi: Elink, osti_id: OstiID, new_values: dict
 ) -> RecordResponse:
     """
@@ -97,33 +93,10 @@ def update_existing_osti_record(
     return elinkapi.update_record(
         osti_id, record_on_elink, state="save"
     )  # user should use update_state_of_osti_record to submit instead
-=======
-    elinkapi: Elink,
-    osti_id: OstiID,
-    new_values: dict
-) -> RecordResponse:
-    record_on_elink = elinkapi.get_single_record(osti_id)
-
-    for keyword in new_values.keys():
-        try:
-            setattr(record_on_elink, keyword, new_values[keyword])
-        except ValueError:
-            print("Extraneous keywords found in the dictionary that do not correspond to attributes in the ELink API's record class.")
-
-    # assume the use with fix the sponsor identifier bug before calling the update function
-    # # fix the issue with the sponsor organization's identifiers
-    # for entry in record_on_elink.organizations:
-    #     if entry.type == "SPONSOR":
-    #         entry.identifiers = [{"type": 'CN_DOE', "value": 'AC02-05CH11231'}]
-    #         break
-
-    return elinkapi.update_record(osti_id, record_on_elink, state="save") # user should use update_state_of_osti_record to submit instead
->>>>>>> 5fa46e4 (Merged upstream (#1))
 
 
 def submit_new_osti_record(
     elinkapi: Elink,
-<<<<<<< HEAD
     new_values: dict,
     state="submit",
 ) -> RecordResponse:
@@ -143,20 +116,12 @@ def submit_new_osti_record(
     new_record = MinimumDARecord(
         **new_values
     )  # record is an instance of the MinimumDARecord model which gives default values to all necessary fields (EXCEPT Title)
-=======
-    new_record: Record,
-    state = "submit", # assuming there is no need to both with saving. just send new record to osti when its ready for submission. also assume bug with DOE contract number identifier in sponsor organization is accounted for
-) -> RecordResponse:
-    # template for all repeated stuff
-    # only submit
->>>>>>> 5fa46e4 (Merged upstream (#1))
     record_response = elinkapi.post_new_record(new_record, state)
 
     return record_response
 
 
 def update_state_of_osti_record(
-<<<<<<< HEAD
     elinkapi: Elink, osti_id: OstiID, new_state: Literal["save", "submit"]
 ) -> RecordResponse:
     """
@@ -188,51 +153,3 @@ def delete_osti_record(elinkapi: Elink, osti_id: OstiID, reason: str) -> bool:
     )
     Validation.handle_response(response)
     return response.status_code == 204  # True if deleted successfully
-=======
-    elinkapi: Elink,
-    osti_id: OstiID,
-    new_state = "submit"
-) -> RecordResponse:
-    record = elinkapi.get_single_record(osti_id)
-
-    # assuming that the user will handle the sponsor identifier bug before calling this function
-    # # fix the issue with the sponsor organization's identifiers
-    # for entry in record.organizations:
-    #     if entry.type == "SPONSOR":
-    #         entry.identifiers = [{"type": 'CN_DOE', "value": 'AC02-05CH11231'}]
-    #         break
-
-    return elinkapi.update_record(osti_id, record, new_state)
-
-
-def delete_osti_record(
-    elinkapi_token: str,
-    osti_id: OstiID,
-    reason: str
-) -> RecordResponse:
-    review_endpoint = "https://review.osti.gov/elink2api/"
-    review_api = Elink(token = elinkapi_token, target=review_endpoint)
-
-    """Delete a record by its OSTI ID."""
-    response = requests.delete(f"{review_api.target}records/{osti_id}?reason={reason}", headers={"Authorization": f"Bearer {review_api.token}"})
-    Validation.handle_response(response)
-    return response.status_code == 204  # True if deleted successfully
-
-def emptyReviewAPI(reason, review_api):
-    allDeleted = True
-    for record in review_api.query_records():
-        delete_osti_record(review_api, record.osti_id, reason)
-
-def make_minimum_record_to_fully_release(
-    title, # required to make record
-    product_type = "DA", # required to make record
-    organizations = [Organization(type='RESEARCHING', name='LBNL Materials Project (LBNL-MP)'), 
-                      Organization(type='SPONSOR', name='TEST SPONSOR ORG', identifiers=[{"type": 'CN_DOE', "value": 'AC02-05CH11231'}])], # sponsor org is necessary for submission
-    persons = [Person(type='AUTHOR', last_name='Perrson')], 
-    site_ownership_code = "LBNL-MP",
-    access_limitations = ['UNL'],
-    publication_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0), # what should this be?
-    site_url = "https://next-gen.materialsproject.org/materials"
-) -> Record:
-    return Record(product_type, title, persons, site_ownership_code, access_limitations, publication_date, site_url)
->>>>>>> 5fa46e4 (Merged upstream (#1))
