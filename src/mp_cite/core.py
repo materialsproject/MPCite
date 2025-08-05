a4ab306cad27eda3994434e00fa6452f30be413b
HugoOnghai <onghaih22@gmail.com>
2025-08-05 16:50:34 -0700

    Addresses comments raised in PR for core.py

--- src/mp_cite/core.py
+++ src/mp_cite/core.py
@@ -8,6 +8,10 @@ import requests
 from elinkapi.utils import Validation
 
 from datetime import datetime
+import pytz
+
+from pydantic import Field
+from typing import List, Literal
 
 OstiID: TypeAlias = int
 
@@ -20,6 +24,18 @@ def find_out_of_date_doi_entries(
     doi_db: str,
     doi_collection: str,
 ) -> list[OstiID]:
+    """
+    find_out_of_date_doi_entries queries MP's mongo collections to find all robocrys documents that were updated less recently than the latest doi document
+
+    :rc_client is the MongoClient used to access the robocrys collection
+    :doi_client is the MongoClient used to access the doi collection (since a new doi collection is planned, they clients are passed separately, though in the future they may be the same client.)
+    :robocrys_db is the name of the database the robocrys collection is in
+    :robocrys_collection is the name of the robocrys collection
+    :doi_db is the name of the database the doi collection is in
+    :doi_collection is the name of the doi collection
+
+    returns a list containing all OSTI IDs associated with out-of-date doi entries.
+    """
     robocrys = rc_client[robocrys_db][robocrys_collection]
     dois = doi_client[doi_db][doi_collection]
 
@@ -54,17 +70,20 @@ def find_out_of_date_doi_entries(
 
 
 def update_existing_osti_record(
-    elinkapi: Elink,
-    osti_id: OstiID,
-    new_values: dict
+    elinkapi: Elink, osti_id: OstiID, new_values: dict
 ) -> RecordResponse:
+    """
+    update_existing_osti_record allows users to provide a dictionary of keywords and new values, which will replace the old values under the same keywords in the record with the given osti id
+
+    :elinkapi is the instance of the elinkapi associated with the environment in which the record is held (e.g. either production or review environment)
+    :osti_id is the osti id of the record which ought to be updated
+    :new_values is a dictionary of keywords (which should exist in ELink's record model) and new value pairs.
+    """
+
     record_on_elink = elinkapi.get_single_record(osti_id)
 
-    for keyword in new_values.keys():
-        try:
-            setattr(record_on_elink, keyword, new_values[keyword])
-        except ValueError:
-            print("Extraneous keywords found in the dictionary that do not correspond to attributes in the ELink API's record class.")
+    for keyword in new_values:
+        setattr(record_on_elink, keyword, new_values[keyword])
 
     # assume the use with fix the sponsor identifier bug before calling the update function
     # # fix the issue with the sponsor organization's identifiers
@@ -73,27 +92,49 @@ def update_existing_osti_record(
     #         entry.identifiers = [{"type": 'CN_DOE', "value": 'AC02-05CH11231'}]
     #         break
 
-    return elinkapi.update_record(osti_id, record_on_elink, state="save") # user should use update_state_of_osti_record to submit instead
+    return elinkapi.update_record(
+        osti_id, record_on_elink, state="save"
+    )  # user should use update_state_of_osti_record to submit instead
 
 
 def submit_new_osti_record(
     elinkapi: Elink,
-    new_record: Record,
-    state = "submit", # assuming there is no need to both with saving. just send new record to osti when its ready for submission. also assume bug with DOE contract number identifier in sponsor organization is accounted for
+    new_values: dict,
+    state="submit",  # assuming there is no need to both with saving. just send new record to osti when its ready for submission. also assume bug with DOE contract number identifier in sponsor organization is accounted for
 ) -> RecordResponse:
+    """
+    submit_new_osti_record generates a new record based on the provided keyword-value pairs in the new_values dict and the default minimum DA Record metadata necessary for submission
+
+    :elinkapi is the elinkapi (see previous)
+    :new_values is the dictionary of keywords and values which want to be included in the submitted record (besides or in lieu of default values). The title MUST be provided.
+    :state defaults to "submit" but the user can simply "save" if desired.
+
+    returns the record response after submission
+    """
+
     # template for all repeated stuff
     # only submit
+    new_record = MinimumDARecord(
+        **new_values
+    )  # record is an instance of the MinimumDARecord model which gives default values to all necessary fields (EXCEPT Title)
     record_response = elinkapi.post_new_record(new_record, state)
 
     return record_response
 
 
 def update_state_of_osti_record(
-    elinkapi: Elink,
-    osti_id: OstiID,
-    new_state = "submit"
+    elinkapi: Elink, osti_id: OstiID, new_state: Literal["save", "submit"]
 ) -> RecordResponse:
     record = elinkapi.get_single_record(osti_id)
+    """
+    update_state_of_osti_record allows a user to update the state of a record with provided osti_id to either "save" or "submit" (the two valid states)
+
+    :elinkapi is the elinkapi (see previous)
+    :osti_id is the OSTI ID associated with the record of which to update state.
+    :new_state is a Literal object, in this case a subtype of strings (either "save" or "submit").
+
+    returns the record response after updating the state.
+    """
 
     # assuming that the user will handle the sponsor identifier bug before calling this function
     # # fix the issue with the sponsor organization's identifiers
@@ -105,30 +146,43 @@ def update_state_of_osti_record(
     return elinkapi.update_record(osti_id, record, new_state)
 
 
-def delete_osti_record(
-    elinkapi: Elink,
-    osti_id: OstiID,
-    reason: str
-) -> RecordResponse:
-    """Delete a record by its OSTI ID."""
-    response = requests.delete(f"{elinkapi.target}records/{osti_id}?reason={reason}", headers={"Authorization": f"Bearer {elinkapi.token}"})
+def delete_osti_record(elinkapi: Elink, osti_id: OstiID, reason: str) -> bool:
+    """
+    Delete a record by its OSTI ID.
+
+    :elinkapi is the elinkapi
+    :osti_id is the osti_id associated with the record which ought to be deleted
+    :reason is a str object which explains in words why the record is to be deleted (necessary for the http request)
+
+    returns true if deleted successfully, else false, which indicates a bad status_code
+    """
+    response = requests.delete(
+        f"{elinkapi.target}records/{osti_id}?reason={reason}",
+        headers={"Authorization": f"Bearer {elinkapi.token}"},
+    )
     Validation.handle_response(response)
     return response.status_code == 204  # True if deleted successfully
 
-def emptyReviewAPI(reason, review_api):
-    allDeleted = True
-    for record in review_api.query_records():
-        delete_osti_record(review_api, record.osti_id, reason)
 
-def make_minimum_record_to_fully_release(
-    title, # required to make record
-    product_type = "DA", # required to make record
-    organizations = [Organization(type='RESEARCHING', name='LBNL Materials Project (LBNL-MP)'), 
-                      Organization(type='SPONSOR', name='TEST SPONSOR ORG', identifiers=[{"type": 'CN_DOE', "value": 'AC02-05CH11231'}])], # sponsor org is necessary for submission
-    persons = [Person(type='AUTHOR', last_name='Perrson')], 
-    site_ownership_code = "LBNL-MP",
-    access_limitations = ['UNL'],
-    publication_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0), # what should this be?
-    site_url = "https://next-gen.materialsproject.org/materials"
-) -> Record:
-    return Record(product_type, title, persons, site_ownership_code, access_limitations, publication_date, site_url)
+class MinimumDARecord(Record):
+    product_type: str = Field(default="DA")
+    title: str  # Required
+    organizations: List[Organization] = Field(
+        default_factory=lambda: [
+            Organization(type="RESEARCHING", name="LBNL Materials Project (LBNL-MP)"),
+            Organization(
+                type="SPONSOR",
+                name="TEST SPONSOR ORG",
+                identifiers=[{"type": "CN_DOE", "value": "AC02-05CH11231"}],
+            ),  # sponsor org is necessary for submission
+        ]
+    )
+    persons: List[Person] = Field(
+        default_factory=lambda: [Person(type="AUTHOR", last_name="Persson")]
+    )
+    site_ownership_code: str = Field(default="LBNL-MP")
+    access_limitations: List[str] = Field(default_factory=lambda: ["UNL"])
+    publication_date: datetime = Field(
+        default_factory=lambda: datetime.now(tz=pytz.UTC)
+    )
+    site_url: str = Field(default="https://next-gen.materialsproject.org/materials")
