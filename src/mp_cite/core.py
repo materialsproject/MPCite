c725d0fffe2a8270abc7dd752e3c0e39ed1196ce
HugoOnghai <onghaih22@gmail.com>
2025-08-07 11:44:37 -0700

    Removed extra files in tests/ and updated email on pyproject.toml

--- src/mp_cite/core.py
+++ src/mp_cite/core.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from typing import Literal, TypeAlias
 
 import requests
@@ -7,6 +8,21 @@ from elinkapi.utils import Validation
 from pymongo import MongoClient
 
 from mp_cite.models import MinimumDARecord
+=======
+from typing import TypeAlias
+
+from elinkapi import Elink
+from elinkapi.record import RecordResponse
+from pymongo import MongoClient
+
+import requests
+from elinkapi.utils import Validation
+
+
+from models import MinimumDARecord
+
+from typing import Literal
+>>>>>>> 0346ff4 (Removed extra files in tests/ and updated email on pyproject.toml)
 
 OstiID: TypeAlias = int
 
