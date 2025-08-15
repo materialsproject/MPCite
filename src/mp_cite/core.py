e2bd3fb68fccdb3ddb5017ee858d0dc0097408bf
HugoOnghai <onghaih22@gmail.com>
2025-08-15 16:28:46 -0700

    Cleaning up for final merge

--- src/mp_cite/core.py
+++ src/mp_cite/core.py
@@ -107,16 +107,21 @@ def update_existing_osti_record(
     Instead, we leave this for the user.
     """
 
-    # record_on_elink = elinkapi.get_single_record(osti_id)
+    record_on_elink = elinkapi.get_single_record(osti_id)
 
-    # for keyword in new_values:
-    #     setattr(record_on_elink, keyword, new_values[keyword])
+    for keyword in new_values:
+        setattr(record_on_elink, keyword, new_values[keyword])
 
-    # return elinkapi.update_record(
-    #     osti_id, record_on_elink, state="save"
-    # )  # user should use update_state_of_osti_record to submit instead
+    if new_state == "submit":
+        # due to bug in elinkapi version<=0.5.2, new_state passing does not update workflow status.
+        # for now, it is updated manually to 'SO' submitted to OSTI, which is should do itself.
+        record_on_elink.workflow_status = "SO"
 
-    elinkapi.patch_record(osti_id, new_values, new_state)
+    return elinkapi.update_record(osti_id, record_on_elink, state=new_state)
+
+    # due to bug in elinkapi version<=0.5.2, elinkapi.patch_record fails due to bad casting of dict to str.
+    # when the next release fixes this, we can change it to this again below.
+    # elinkapi.patch_record(osti_id, new_values, new_state)
 
 
 def submit_new_osti_record(
